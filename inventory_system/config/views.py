a9304f9252bd467c2aa4456fd3f373ab925427d4
aashishChakra <ashishchakradhar00@gmail.com>
2024-03-04 23:27:42 +0545

    added bootstrap codes

--- inventory_system/config/views.py
+++ inventory_system/config/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< HEAD
+=======
+def home(request):
+    return render(request,'home.html')
+>>>>>>> 55b0b7e (added bootstrap codes)
