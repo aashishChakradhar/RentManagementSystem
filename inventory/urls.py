7e652c6f3cf54c756e994bb47194274a59352e6a
aashishChakra <ashishchakradhar00@gmail.com>
2024-03-22 21:11:53 +0545

    changes made in login and register form

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
+    path('login', views.loginUser, name='login'),
+    path('signup', views.register, name='signup'),
 ]
