01f57414e003553460c92dff2e6c12f675fb40f4
aashishChakradhar <ashishchakradhar00@gmail.com>
2024-12-25 17:47:01 +0545

    add new schema

--- rent/models.py
+++ rent/models.py
@@ -14,11 +14,22 @@ class BaseModel(models.Model):
     class Meta:
         abstract=True
 
+class Building(BaseModel):
+    building_name = models.CharField(max_length=50)
+    building_address = models.TextField()
+    number_of_rooms = models.IntegerField()
+    building_type = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.building_name} in {self.building_address}'
+
 class Room(BaseModel):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
     room_number = models.IntegerField(default=1)
     room_name = models.CharField(max_length=30, default='Room')
     rent_amount = models.DecimalField(decimal_places=2,max_digits=6, default=3000.00)
-    available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
     def __str__(self):
         return f"{self.room_number} - {self.room_name}"
 
@@ -27,14 +38,19 @@ class Room(BaseModel):
 
 class Payment(BaseModel):
     room_no = models.ForeignKey(Room, related_name='Room_History', on_delete=models.CASCADE)
-    # individual = models.ForeignKey(Individual,on_delete=models.CASCADE)
     recieved_amount = models.DecimalField(decimal_places=2,max_digits=6,default=0.00)
+    
+    recieved_date = models.DateField()
+
     recieved_month = models.IntegerField(default=1)
     recieved_year = models.IntegerField(default=2081)
     recieved_data = models.IntegerField(default=1)
     # amount_remaining = models.DecimalField(decimal_places=2, max_digits=6,default=0.00)
     # months_remaining = models.IntegerField(default=0)
     remarks = models.TextField(default=None)
+
+    updated_at = models.DateField()
+
     def __str__(self):
         return f"{self.room_no.room_number} - {self.room_no.room_name}"
 
