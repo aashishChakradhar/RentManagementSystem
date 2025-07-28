920e7e2071503f9082910ca8dd6e6295243e7544
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-07-28 17:35:14 +0545

    added user id for base model

--- rent/models.py
+++ rent/models.py
@@ -9,6 +9,7 @@ import uuid
 
 class BaseModel(models.Model):
     uid=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     created_at=models.DateField(auto_now_add=True)
     updated_at=models.DateField(auto_now=True)
     class Meta:
